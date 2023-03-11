f170cfb50c2a160abc5e5f8469a3af039be49193
Clicker00 <suwinsky@wp.pl>
2023-03-11 02:59:00 +0100

    Delete tiers view as unnecessary

--- images_api/urls.py
+++ images_api/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import AccountTierApiView, ImageView
 
 urlpatterns = [
-    path('images', ImageView.as_view()),
-    path('tiers', AccountTierApiView.as_view())
+    path('images', ImageView.as_view())
 ]
